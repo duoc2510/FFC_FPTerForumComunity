fa8fc4a2cd37a829e16eaf16592bbce62f13ab24
duoc2510 <duocnlt2510@gmail.com>
2024-05-22 11:03:31 +0700

    DOI CAU TRUNG SANG MAVEN

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -10,15 +10,5 @@ package util;
  */
 public class Constants {
 
-    public static String GOOGLE_CLIENT_ID = "244022125519-87q2unlgmf45kmgam5707eda4480fr4s.apps.googleusercontent.com";
-
-    public static String GOOGLE_CLIENT_SECRET = "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";
-
-    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/FPTer/logingooglehandler";
-
-    public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
-
-    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
-
-    public static String GOOGLE_GRANT_TYPE = "authorization_code";
+  
 }
