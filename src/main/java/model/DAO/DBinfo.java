09f3cf88464edeb4987129ac92a95074bb716beb
Dimdim <nguyendiem17042003@gmail.com>
2024-06-22 15:30:33 +0700

    finish event

--- src/main/java/model/DAO/DBinfo.java
+++ src/main/java/model/DAO/DBinfo.java
@@ -2,7 +2,7 @@ package model.DAO;
 
 public interface DBinfo {
 
-    String dbURL = "jdbc:sqlserver://LAPTOP-PN9QOBAM:1433;databaseName=FFCFPTerForumComunity;encrypt=true;trustServerCertificate=true;";
+    String dbURL = "jdbc:sqlserver://PT365SU-VMN90Q6:1433;databaseName=FFCFPTerForumComunity;encrypt=true;trustServerCertificate=true;";
     String dbUser = "sa";
     String dbPass = "123";
     String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
