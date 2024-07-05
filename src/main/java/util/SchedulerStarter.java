f289e2c0d46ccbd511dc0e8a8674cc9c49e56450
CEL Soft <phuchoangnguyen18012003@gmail.com>
2024-07-05 14:39:16 +0700

    phuc cai tien shop

--- src/main/java/util/SchedulerStarter.java
+++ src/main/java/util/SchedulerStarter.java
@@ -29,7 +29,7 @@ public class SchedulerStarter implements ServletContextListener {
 
             // Set the date and time for the trigger
             Calendar calendar = Calendar.getInstance();
-            calendar.set(2024, Calendar.JUNE, 29, 18, 10, 0);
+            calendar.set(2024, Calendar.JULY, 5, 14, 27, 0);
             Date triggerDate = calendar.getTime();
 
             Trigger trigger = TriggerBuilder.newTrigger()
@@ -51,3 +51,28 @@ public class SchedulerStarter implements ServletContextListener {
         }
     }
 }
+
+//public class SchedulerStarter implements ServletContextListener {
+//
+//    public void contextInitialized(ServletContextEvent sce) {
+//        try {
+//            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+//            JobDetail job = JobBuilder.newJob(RankResetJob.class).build();
+//            Trigger trigger = TriggerBuilder.newTrigger()
+//                    .withSchedule(monthlyOnDayAndHourAndMinute(1, 0, 0)) // Reset at 00:00 on the first day of the month
+//                    .build();
+//            scheduler.scheduleJob(job, trigger);
+//            scheduler.start();
+//        } catch (SchedulerException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public void contextDestroyed(ServletContextEvent sce) {
+//        try {
+//            StdSchedulerFactory.getDefaultScheduler().shutdown();
+//        } catch (SchedulerException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
