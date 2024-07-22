0d6dece64faf14782d88b7ae6a994908b95ca2e1
TrungYone <maconca1234@gmail.com>
2024-07-22 16:03:19 +0700

    update avatar handel report

--- src/main/java/model/DAO/DBinfo.java
+++ src/main/java/model/DAO/DBinfo.java
@@ -2,8 +2,8 @@ package model.DAO;
 
 public interface DBinfo {
 
-    String dbURL = "jdbc:sqlserver://PT365SU-VMN90Q6:1433;databaseName=FFCFPTerForumComunity;encrypt=true;trustServerCertificate=true;";
+    String dbURL = "jdbc:sqlserver://LAPTOP-6AJRBEUP:1433;databaseName=FFCFPTerForumComunity;encrypt=true;trustServerCertificate=true;";
     String dbUser = "sa";
-    String dbPass = "123";
+    String dbPass = "0934077041";
     String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
 }
